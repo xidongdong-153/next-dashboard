0ccf40d710382811486f76030afbcbe4e990dfbc
xidongdong-153 <youzegehq@gmail.com>
2023-11-22 20:29:18 +0800

    第一次提交

---
+++ app/ui/acme-logo.tsx
@@ -0,0 +1,13 @@
+import { JetBrainsMono } from '@/app/ui/fonts';
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
+
+export default function AcmeLogo() {
+  return (
+    <div
+      className={`${JetBrainsMono.className} flex flex-row items-center leading-none text-white`}
+    >
+      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+      <p className="text-[44px]">Acme</p>
+    </div>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, JetBrains_Mono } from 'next/font/google';
+
+export const inter = Inter({ weight: ['400', '700'], subsets: ['latin'] });
+
+export const JetBrainsMono = JetBrains_Mono({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
